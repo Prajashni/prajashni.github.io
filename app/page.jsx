eee1d82214970252545006111cb22866efcc9e50
Prajashni <85519637+Prajashni@users.noreply.github.com>
2025-06-20 17:50:30 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import Portfolio from '@/components/Portfolio';
+import Portfolio from '../components/Portfolio';
 
 export default function Home() {
   return <Portfolio />;
