22fbacc33bf25e8e10ee5e31e518a0462392db52
Prajashni <85519637+Prajashni@users.noreply.github.com>
2025-06-20 16:55:38 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import Portfolio from '../components/Portfolio';
+import Portfolio from "../components/Portfolio";
 
 export default function Home() {
   return <Portfolio />;
